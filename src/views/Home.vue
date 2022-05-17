17446f009032eece8c0dea135d681649bb958f59
hafiz-aa <mhafiz.aa@gmail.com>
2022-05-17 11:09:09 +0700

    commit on master

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est mollitia in nobis eveniet ipsam aspernatur voluptatem ad eum ut harum? Nam quia nemo, aliquid minima quae deleniti veniam illum ipsam assumenda cumque maiores fugiat rerum distinctio neque nulla accusamus suscipit laudantium. Enim ab fugiat iusto ducimus sunt eligendi illo? Reprehenderit, voluptatibus minima.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est mollitia in nobis eveniet ipsam aspernatur voluptatem ad eum ut harum? Nam quia nemo, aliquid minima quae deleniti veniam illum ipsam assumenda cumque maiores fugiat rerum distinctio neque nulla accusamus suscipit laudantium. Enim ab fugiat iusto ducimus sunt eligendi illo? Reprehenderit, voluptatibus minima.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
